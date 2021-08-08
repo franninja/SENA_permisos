e5003ae77c8e7fc658b35a805eb45525d9d06790
AlexanderB43S <84984022+AlexanderB43S@users.noreply.github.com>
2021-08-08 13:23:13 -0500

    estructura de creacion de desafio

--- resources/views/idea/prueba.blade.php
+++ resources/views/idea/prueba.blade.php
@@ -9,51 +9,90 @@
 @section('content')
 <div class="container-fluid">
 
-    <div class="card">
-         <div>
-         <form action="Crear"  method="POST" enctype="multipart/form-data">
-                <div class="row">
-                    <div class="col">
-                        <Label>ACA VA UNA PREGUNTA</Label>
-                        <hr>
-                        <Label>ACA VA UNA PREGUNTA</Label>
-                        <hr>
-                        <Label>ACA VA UNA PREGUNTA</Label>
-                        <hr>
-                        <Label>ACA VA UNA PREGUNTA</Label>
-                        <hr>
-                        <Label>ACA VA UNA PREGUNTA</Label>
-                        <hr>
-                        <Label>ACA VA UNA PREGUNTA</Label>
-                        <hr>
-                        <LAbel><B>Preguntas Adicionales</B></LAbel>
-                        <Input type="text" class="form-control" placeholder="Escribe la pregunta aquí" aria-label="Nameidea"></Input>
-                    </div>
+    <div class="card card-secondary">
+              <div class="card-header">
+                <h3 class="card-title">Asignar preguntas para el desafio</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <form method="POST" action="{{ route('challenge.store') }}" enctype="multipart/form-data">
+                  <div class="row">
+                    <div class="col-sm-6">
 
-                    <div class="col">
-                        <Label>espacio para activar/Desactivar pregunta</Label>
-                        <hr>
-                        <Label>espacio para activar/Desactivar pregunta</Label>
-                        <hr>
-                        <Label>espacio para activar/Desactivar pregunta</Label>
-                        <hr>
-                        <Label>espacio para activar/Desactivar pregunta</Label>
-                        <hr>
-                        <Label>espacio para activar/Desactivar pregunta</Label>
-                        <hr>
-                        <Label>espacio para activar/Desactivar pregunta</Label>
-                        <hr>
-                        <LAbel><B>espacio para activar/Desactivar pregunta</B></LAbel>
+
+                        <div class="form-group">
+                            <label for="name">Nombre del desafio</label>
+                            <input id="name" type="text" placeholder="nombre del desafio" class="form-control">
+                        </div>
+
+
+                        <div class="form-group">
+                            <!-- Area a la cual se asignara el desafio -->
+                            <label for="privacity">Que tipo de area podra ver el desafio?</label>
+                            <select class="custom-select rounded-0" name="privacity" id="privacity">
+                                <option >-Seleccionar</option>
+                                <option>area 1</option>
+                                <option>area 2</option>
+                                <option>area 3</option>
+                                <option>Todas las areas</option>
+                            </select>
+                            <!--  /. Area a la cual se asignara el desafio -->
+                        </div>
+
+
+                      <!-- checkbox - switch -->
+                        <div class="form-group">
+
+                            <div class="custom-control custom-switch custom-switch-off-danger custom-switch-on-success">
+                                <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                                <label for="customSwitch1" class="custom-control-label">Primera pregunta (Ej: Nombre de la idea)</label>
+                            </div>
+                            <div class="custom-control custom-switch custom-switch-off-danger custom-switch-on-success">
+                                <input type="checkbox" class="custom-control-input" id="customSwitch2">
+                                <label for="customSwitch2" class="custom-control-label">Segunda pregunta (Ej: Descripción)</label>
+                            </div>
+                            <div class="custom-control custom-switch custom-switch-off-danger custom-switch-on-success">
+                                <input type="checkbox" class="custom-control-input" id="customSwitch3">
+                                <label for="customSwitch3" class="custom-control-label">Tercera pregunta (Ej: Presupuesto)</label>
+                            </div>
+                            <div class="custom-control custom-switch custom-switch-off-danger custom-switch-on-success">
+                                <input  type="checkbox" class="custom-control-input" id="customSwitch4">
+                                <label for="customSwitch4" class="custom-control-label">Cuarta pregunta (Ej: Tiempo de ejecución)</label>
+                            </div>
+                            <div class="custom-control custom-switch custom-switch-off-danger custom-switch-on-success">
+                                <input type="checkbox" class="custom-control-input" id="customSwitch5">
+                                <label for="customSwitch5" class="custom-control-label">Quinta pregunta (Ej: Antecedentes de la idea)</label>
+                            </div>
+                            
+                        </div>
+
+                       <!-- /. checkbox - switch -->
+                        </div>
+
+                  </div>
+
+
+                  <div>
+                    <div>
+                      <label>Si quiere agregar mas preguntas, Escribalas en un documento de texto y arrastralas aqui</label>
+                    </div>
+                  </div>
+
+
+                </form>
+
+                <div class="row" style="clear: both;margin-top: 18px;">
+                    <div class="col-12">
+                        <div class="dropzone" id="myDropzone"></div>
                     </div>
                 </div>
-         
-         </form>
-         </div>
+              </div>
+
+              <div class="card-footer">
+                    <button type="submit" id="submit-all" class="btn btn-primary">Crear Desafio</button>
+              </div>
+              <!-- /.card-body -->
     </div>
- 
-
-
-
 
 
 
