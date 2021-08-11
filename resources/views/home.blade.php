0b6cc71ff5aa3b0719b79c222f2a77c93edf0fba
AlexanderB43S <84984022+AlexanderB43S@users.noreply.github.com>
2021-08-11 12:06:15 -0500

    cambio minimo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,14 +18,14 @@
         <!-- small box -->
         <div class="small-box bg-info">
           <div class="inner">
-            <h3>150</h3>
+            <h3>10</h3>
 
-            <p>New Orders</p>
+            <p>Desafios Activos</p>
           </div>
           <div class="icon">
             <i class="ion ion-bag"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          <a href="#" class="small-box-footer">Más Información<i class="fas fa-arrow-circle-right"></i></a>
         </div>
       </div>
       <!-- ./col -->
@@ -33,14 +33,14 @@
         <!-- small box -->
         <div class="small-box bg-success">
           <div class="inner">
-            <h3>53<sup style="font-size: 20px">%</sup></h3>
+            <h3>5<sup style="font-size: 20px"></sup></h3>
 
-            <p>Bounce Rate</p>
+            <p>Desafios Resueltos</p>
           </div>
           <div class="icon">
             <i class="ion ion-stats-bars"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          <a href="#" class="small-box-footer">Más Información <i class="fas fa-arrow-circle-right"></i></a>
         </div>
       </div>
       <!-- ./col -->
@@ -48,14 +48,14 @@
         <!-- small box -->
         <div class="small-box bg-warning">
           <div class="inner">
-            <h3>{{ count($users) }}</h3>
+            <h3>15</h3>
 
-            <p>Usuarios Registrados</p>
+            <p>Ideas Enviadas</p>
           </div>
           <div class="icon">
             <i class="ion ion-person-add"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          <a href="#" class="small-box-footer">Más Información <i class="fas fa-arrow-circle-right"></i></a>
         </div>
       </div>
       <!-- ./col -->
@@ -63,14 +63,14 @@
         <!-- small box -->
         <div class="small-box bg-danger">
           <div class="inner">
-            <h3>65</h3>
+            <h3>{{ count($users) }}</h3>
 
-            <p>Unique Visitors</p>
+            <p>Usuarios Registrados</p>
           </div>
           <div class="icon">
             <i class="ion ion-pie-graph"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          <a href="#" class="small-box-footer">Más Información <i class="fas fa-arrow-circle-right"></i></a>
         </div>
       </div>
       <!-- ./col -->
--- resources/views/idea/prueba.blade.php
+++ resources/views/idea/prueba.blade.php
@@ -16,6 +16,8 @@
               <!-- /.card-header -->
               <div class="card-body">
                 <form method="POST" action="{{ route('challenge.store') }}" enctype="multipart/form-data">
+
+                 @csrf
                   <div class="row">
                     <div class="col-sm-6">
 
